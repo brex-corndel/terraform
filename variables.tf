d4fbfd68f5f37ddd6550a3633865555f37e7b8be
brex-corndel <71491954+brex-corndel@users.noreply.github.com>
2022-02-07 21:40:39 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
-compartment_ocid	ocid1.tenancy.oc1..aaaaaaaaabeclzoshd6jv4duinduq666wyb2ajrsxjtinz6klf2nxxmfm53a	compartment_ocid
-region	uk-london-1	region
-tenancy_ocid	ocid1.tenancy.oc1..aaaaaaaaabeclzoshd6jv4duinduq666wyb2ajrsxjtinz6klf2nxxmfm53a
+variable "compartment_ocid" {
+  type        = string
+  description = "ocid1.tenancy.oc1..aaaaaaaaabeclzoshd6jv4duinduq666wyb2ajrsxjtinz6klf2nxxmfm53a"
+}
+
+variable "region" {
+  type        = string
+  description = "uk-london-1"
+}
+
+variable "tenancy_ocid" {
+  type        = string
+  description = "ocid1.tenancy.oc1..aaaaaaaaabeclzoshd6jv4duinduq666wyb2ajrsxjtinz6klf2nxxmfm53a"
+}
